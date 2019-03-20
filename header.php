deaa199e9c185dcde27c3aff223b29e8c519d53b
Shamus Osler <shamuso23@gmail.com>
2019-03-20 14:45:14 -0500

    fixed nav to work with new css framework

--- header.php
+++ header.php
@@ -1,25 +1,34 @@
 <?php session_start() ?>
 <header>
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="index.php">Home</a>
-            </li>
-            <?php if(isset($_SESSION["USERID"])): ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="actions/signout.php">Signout</a>
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li class="">
+                    <a class="" href="index.php">Home</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="  createpost.php">Post</a>
+                <li class=" ">
+                    <a class="" href="createpost.php">Post</a>
                 </li>
-            <?php else: ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="login.php">Login</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="signup.php">Signup</a>
-                </li>
-            <?php endif ?>
-        </ul>
+            </ul>
+        </div>
+            <div class="uk-navbar-right">
+                <ul class="uk-navbar-nav">
+                    <?php if(!isset($_SESSION["USERID"])): ?>
+                        <li>
+                            <a href="login.php">Login</a>
+                            <div class="uk-navbar-dropdown">
+                            <ul class="uk-nav uk-navbar-dropdown-nav">
+                                <li><a href="login.php">Login</a></li>
+                                <li><a href="signup.php">Sign up</a></li>
+                            </ul>
+                            </div>
+                        </li>
+                    <?php else: ?>
+                    <li>
+                        <a href="actions/signout.php">Signout</a>
+                    </li>
+                </ul>
+            </div>            
+        <?php endif ?>
     </nav>
 </header>
