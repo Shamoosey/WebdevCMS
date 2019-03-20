6107f9892035407ea8c68c45c2ad97962ea01814
Shamus Osler <shamuso23@gmail.com>
2019-03-20 14:06:21 -0500

    indent header

--- header.php
+++ header.php
@@ -6,16 +6,16 @@
                 <a class="nav-link" href="index.php">Home</a>
             </li>
             <?php if(isset($_SESSION["USERID"])): ?>
-            <li class="nav-item">
-                <a class="nav-link" href="actions/signout.php">Signout</a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="actions/signout.php">Signout</a>
+                </li>
             <?php else: ?>
-            <li class="nav-item">
-                <a class="nav-link" href="login.php">Login</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="signup.php">Signup</a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signup.php">Signup</a>
+                </li>
             <?php endif ?>
         </ul>
     </nav>
