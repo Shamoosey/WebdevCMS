769a6018a17931c76c9f24eb2be090475d38105c
Shamus Osler <shamuso23@gmail.com>
2019-03-24 19:47:17 -0500

    added searchbar to index

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <div class="uk-background-fixed uk-background-center-center uk-height-viewport" style="background-image: url(assets/images/index-background.jpg);">  
     <?php require "header.php" ?>
     <h1 class="uk-text-center"><span>Explore Manitoba</span></h1>
+    <div class="uk-flex uk-flex-center">
+        <input class="uk-input uk-form-width-large" type="text" id="search" placeholder="Search for a keyword"/>
+        <button class="uk-button-secondary uk-margin-left">Search</button>
+    </div>
     </div>
     <div class="row">
         <div class="col uk-margin uk-card uk-card-default uk-card-body">
