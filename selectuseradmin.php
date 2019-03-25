4b8d631c08dd6584f098666309ff8facff00a64c
Shamus Osler <shamuso23@gmail.com>
2019-03-25 10:33:57 -0500

    added validation so only admins can delete users

--- selectuseradmin.php
+++ selectuseradmin.php
@@ -1,11 +1,25 @@
 <?php   
-    $userID = filter_input(INPUT_GET, "userid", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    require "actions/connect.php";
+    $validUser = false;
+    session_start();
+    if(isset($_SESSION["USERID"])){
+        if($_SESSION["ADMIN"] == 1){
+            $validUser = true;
+        }
+    }
+    if($validUser){
 
-    $query = $db -> prepare("SELECT * FROM users WHERE UserID = '$userID'");
-    $query -> execute();
-    $user = $query -> fetchAll();
-    $username =ucfirst(strtolower($user[0]["Username"]));
+        $userID = filter_input(INPUT_GET, "userid", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        require "actions/connect.php";
+    
+        $query = $db -> prepare("SELECT * FROM users WHERE UserID = '$userID'");
+        $query -> execute();
+        $user = $query -> fetchAll();
+        $username =ucfirst(strtolower($user[0]["Username"]));
+
+    } else {
+        header("location: index.php");
+    }
+    session_abort();
 ?>
 <!DOCTYPE html>
 <html>
